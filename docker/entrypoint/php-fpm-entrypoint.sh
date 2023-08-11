af51d4e644d70b0dd4123d53d980f6df03950fba
condrici <condrici@gmail.com>
2023-08-11 13:34:33 +0200

    Update comment

--- docker/entrypoint/php-fpm-entrypoint.sh
+++ docker/entrypoint/php-fpm-entrypoint.sh
@@ -3,5 +3,5 @@
 COMPOSER_MEMORY_LIMIT=-1 composer install
 php artisan key:generate
 
-# Fix docker container exiting with code 0
+# Fix docker container exiting with code 0 (when running docker-compose up without -d)
 tail -f /dev/null
